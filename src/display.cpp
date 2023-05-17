127125ca9c1eecb9fd60453c457704b46b3d8545
vozarv <viktor.vozar@propatria-inc.com>
2023-05-17 12:37:20 +0200

    Added Anti-Aliasing

--- src/display.cpp
+++ src/display.cpp
@@ -3,8 +3,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_video.h>
+#include "GLFW/glfw3.h"
 
-Display::Display(int width, int height, const std::string &title) {
+Display::Display(int width, int height, const std::string &title)
+{
 
   SDL_Init(SDL_INIT_EVERYTHING);
 
@@ -15,6 +17,7 @@ Display::Display(int width, int height, const std::string &title) {
   SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16);
 
   m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
                               SDL_WINDOWPOS_CENTERED, width, height,
@@ -23,7 +26,8 @@ Display::Display(int width, int height, const std::string &title) {
 
   GLenum status = glewInit();
 
-  if (status != GLEW_OK) {
+  if (status != GLEW_OK)
+  {
     std::cerr << "Glew failed to initialize!" << std::endl;
   }
 
@@ -37,7 +41,10 @@ Display::Display(int width, int height, const std::string &title) {
 
   // Blending
   glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  // Anti-Aliasing
+  glEnable(GL_MULTISAMPLE);
 
   SDL_SetWindowResizable(m_window, SDL_TRUE);
 
@@ -45,19 +52,22 @@ Display::Display(int width, int height, const std::string &title) {
   m_height = height;
 }
 
-Display::~Display() {
+Display::~Display()
+{
 
   SDL_GL_DeleteContext(m_glContext);
   SDL_DestroyWindow(m_window);
   SDL_Quit();
 }
 
-void Display::Update() {
+void Display::Update()
+{
   SDL_GL_SwapWindow(m_window);
 
   SDL_Event e;
 
-  while (SDL_PollEvent(&e)) {
+  while (SDL_PollEvent(&e))
+  {
     if (e.type == SDL_QUIT)
       m_is_closed = true;
   }
@@ -67,7 +77,8 @@ void Display::Close() { m_is_closed = true; }
 
 bool Display::IsClosed() { return m_is_closed; }
 
-void Display::Clear(float r, float g, float b, float a) {
+void Display::Clear(float r, float g, float b, float a)
+{
   glClearColor(r, g, b, a);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
