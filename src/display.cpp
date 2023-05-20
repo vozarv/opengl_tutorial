fa532f3c5086d5701f88ff8c069c89bf7b94d0bd
Vozár Viktor <viktor.vozar@electronembedded.com>
2023-05-20 13:26:26 +0200

    Solved Anti-Aliasing bug on Windows

--- src/display.cpp
+++ src/display.cpp
@@ -17,7 +17,7 @@ Display::Display(int width, int height, const std::string &title)
   SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16);
+  SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 
   m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
                               SDL_WINDOWPOS_CENTERED, width, height,
