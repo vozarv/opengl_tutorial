28d0d8d036aa95e1e5f48bba0379f0132a281b1d
vozarv <viktor.vozar@propatria-inc.com>
2023-10-18 09:55:23 +0200

    Solve RenderDoc incompatibility

--- src/display.cpp
+++ src/display.cpp
@@ -19,6 +19,9 @@ Display::Display(int width, int height, const std::string &title)
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
   m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
                               SDL_WINDOWPOS_CENTERED, width, height,
                               SDL_WINDOW_OPENGL);
